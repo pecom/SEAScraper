fe0646b75643288ef4fb84d0f688bb1055b11112
pecom <padari@raleighcharterhs.org>
2016-11-02 11:05:46 -0400

    Hrefs
    Gets the hrefs

--- seaScraper.js
+++ seaScraper.js
@@ -1,22 +1,29 @@
 var Nightmare = require('nightmare');
 var cheerio = require('cheerio');
-var nightmare = Nightmare({ show: false });
+var deptLinks = [];
 
 
-
+hrefScraping();
 
 
 function hrefScraping(){
-  new Nightmare().goto('http://bulletin.engineering.columbia.edu/key-course-listings')
-  .evaluate(function(){
-    return document.querySelector('#block-menu_block-1').innerHTML + document.querySelector('.colb ul').innerHTML;
-  })
-  .end()
-  .then(function(htmlThingo){
-    $ = cheerio.load(htmlThingo);
-    
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
+  console.log("doing the link scrape");
+  new Nightmare()
+    .goto('http://bulletin.engineering.columbia.edu/key-course-listings')
+    .evaluate(function (){
+      return document.querySelector('#block-menu_block-1').innerHTML;
+    })
+    .end()
+    .then(function(htmlThingo){
+      $ = cheerio.load(htmlThingo);
+      $('li', '.content').each(function(i, elem){
+        deptLinks[i] = $('a', elem).attr('href');
+      });
+      deptLinks.splice(1, 1);             //Get rid of "key-course-listings"
+      
+    })
+    .catch(function (error) {
+      console.error('Search failed:', error);
+    });
+
 }
